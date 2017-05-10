4ec62fb5c3c64609dcff4c17472b42f28f82d2ed
Marie-Darline Mroß <marie-d.mross@web.de>
2017-05-10 13:40:46 +0200

    Implemented simple demo for deployment

---
+++ HoloLens/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    public Vector3 RotationVector;
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(RotationVector.x * Time.deltaTime, RotationVector.y * Time.deltaTime, RotationVector.z * Time.deltaTime, Space.Self);
+	}
+}
