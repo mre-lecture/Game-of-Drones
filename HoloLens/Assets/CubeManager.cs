ce08f17a40c1f46c04e84e3c3323ed97d72d8669
seb1234 <seb.schuler@gmx.de>
2017-05-17 12:37:40 +0200

    create prefabs on runtime
    Prefabs können mit AirTap während der Laufzeit erzeugt werden.

---
+++ HoloLens/Assets/CubeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class CubeManager : MonoBehaviour {
+
+    public GameObject blueCubePrefab;
+
+    GestureRecognizer recognizer;
+
+	// Use this for initialization
+	void Start () {
+        recognizer = new GestureRecognizer();
+
+        recognizer.TappedEvent += Recognizer_TappedEvent;
+
+        recognizer.StartCapturingGestures();
+	}
+
+    private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay) {
+        var direction = headRay.direction;
+        var origin = headRay.origin;
+        var position = origin + direction * 2.0f;
+
+        Instantiate(blueCubePrefab, position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
