85beaad9ebd1cfe2b08b7351499f7e3fbe5989de
seb1234 <seb.schuler@gmx.de>
2017-06-18 19:46:44 +0200

    Dronensteuerung

---
+++ openURL/Assets/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    GameObject drone;
+    
+	void Start () {
+        drone = GameObject.FindGameObjectWithTag("drone");
+
+    }
+	
+	void Update () {
+        var cameraTransform = Camera.main.transform;
+        transform.LookAt(drone.transform);
+        this.transform.position += cameraTransform.forward * 0.01f;
+        this.transform.position += cameraTransform.right * 0.01f;
+
+        //this.transform.RotateAround(cameraTransform.position, cameraTransform.up, 5f);
+        //this.transform.Rotate(Vector3.up, 20 * Time.deltaTime);
+
+    }
+}
